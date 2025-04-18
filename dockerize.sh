34645197aa59d45270d18ada2977e2fca7899c64
Georgi Chalakov <gchalakovmmi@gmail.com>
2025-04-18 11:31:06 +0300

    Removed port in dockerize.sh

--- dockerize.sh
+++ dockerize.sh
@@ -2,4 +2,4 @@ name="zaivod-logistics"
 docker rm -f $name
 docker rmi $name
 docker build -t $name .
-docker run -d -p 8080:8080 --network net -e VIRTUAL_HOST="logistics.zaivod.com" -e LETSENCRYPT_HOST="logistics.zaivod.com" --name $name $name
+docker run -d --network net -e VIRTUAL_HOST="logistics.zaivod.com" -e LETSENCRYPT_HOST="logistics.zaivod.com" --name $name $name
