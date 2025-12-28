4bc0cd640078d86d63cbd38f19515a4ef4ec98d7
Georgi Chalakov <gchalakovmmi@gmail.com>
2025-12-29 00:52:45 +0200

    fix: change text/plane to text/html for google verification

--- internal/handlers/google-verification.go
+++ internal/handlers/google-verification.go
@@ -6,7 +6,7 @@ import (
 
 func GoogleVerification() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain")
+		w.Header().Set("Content-Type", "text/html")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("google-site-verification: google3d4b8e47f66457f9.html"))
 	})
