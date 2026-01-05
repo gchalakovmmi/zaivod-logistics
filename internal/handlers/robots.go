bdc46a287cb3b7a6f7403b70b64d3bba133d6521
Georgi Chalakov <gchalakovmmi@gmail.com>
2026-01-05 14:31:50 +0200

    fix: fix typo in Content-Type

--- internal/handlers/robots.go
+++ internal/handlers/robots.go
@@ -6,7 +6,7 @@ import (
 
 func Robots() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plane")
+		w.Header().Set("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(`User-agent: Googlebot
 Disallow: /nogooglebot/
